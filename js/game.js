8275fd5e06a9852a1aa34dc4bd488015969c5be0
gareththomasnz <gareththomasnz@yahoo.com>
2016-11-20 15:42:45 +1300

    second commit

--- js/game.js
+++ js/game.js
@@ -1,84 +1,84 @@
- (function () {
-   
-window.onload = function() {
-    var btnTw = document.getElementById("twitter-btn");
-    var btnFb = document.getElementById("facebook-btn");
-    var output = document.getElementById("question");
-    var listen = document.getElementById("check");
-    listen.onclick = checkanswer();
-    //btnTw.onclick = twitteropen();
-    //btnFb.onclick = facebookopen();
+(function() {
 
-    //return false;
-//};
- 
- //FB block was here
+	window.onload = function() {
+		var btnTw = document.getElementById("twitter-btn");
+		var btnFb = document.getElementById("facebook-btn");
+		var output = document.getElementById("question");
+		var listen = document.getElementById("check");
+		listen.onclick = checkanswer();
+		//btnTw.onclick = twitteropen();
+		//btnFb.onclick = facebookopen();
 
-//document.getElementById("check").addEventListener("click", checkanswer);
+		//return false;
+		//};
 
-var questionnum = 0,
-	score = 0,
-	totalquestions = 3,
-	gameplaying = true,
-	description = '';
-var quiz = [{
-	"question": "4+4=",
-	"answer": "8"
-}, {
-	"question": "2+2=",
-	"answer": "4"
-}, {
-	"question": "5+1=",
-	"answer": "6"
-}];
+		//FB block was here
 
-//console.log(quiz);
+		//document.getElementById("check").addEventListener("click", checkanswer);
 
-function checkanswer() {
-	if (gameplaying) {
-		var myanswer = document.getElementById("guess").value;
-		if (myanswer == quiz.questionnum.answer) {
-			score++;
+		var questionnum = 0,
+			score = 0,
+			totalquestions = 3,
+			gameplaying = true,
+			description = '';
+		var quiz = [{
+			"question": "4+4=",
+			"answer": "8"
+		}, {
+			"question": "2+2=",
+			"answer": "4"
+		}, {
+			"question": "5+1=",
+			"answer": "6"
+		}];
+
+		//console.log(quiz);
+
+		function checkanswer() {
+			if (gameplaying) {
+				var myanswer = document.getElementById("guess").value;
+				if (myanswer == quiz.questionnum.answer) {
+					score++;
+				}
+				console.log(score);
+				questionnum++;
+			}
+			if (questionnum >= totalquestions) {
+				console.log('end game');
+				output.innerHTML = "game over your score is " + score + " correct out of " + totalquestions;
+				gameplaying = false;
+			} else {
+				setquiz(questionnum);
+			}
 		}
-		console.log(score);
-		questionnum++;
-	}
-	if (questionnum >= totalquestions) {
-		console.log('end game');
-		output.innerHTML = "game over your score is " + score + " correct out of " + totalquestions;
-		gameplaying = false;
-	} else {
+
+		function setquiz(questionnum) {
+			if (gameplaying) {
+				output.innerHTML = quiz[questionnum]['question'];
+			}
+		}
 		setquiz(questionnum);
-	}
-}
 
-function setquiz(questionnum) {
-	if (gameplaying) {
-		output.innerHTML = quiz[questionnum]['question'];
-	}
-}
-setquiz(questionnum);
-
-// Social Sharing Code START
+		// Social Sharing Code START
 
 
 
- btnFb.onclick = function facebookopen() {
-	FB.ui({
-		method: "feed",
-		link: "http://www.discoveryvip.com",
-		picture: "http://www.discoveryvip.com/assets/img/siteimage.jpg",
-		name: "Play the quiz to win",
-		caption: "",
-		description: "I scored " + score + " correct out of " + totalquestions + " questions on the quiz"
-	});
-};
+		btnFb.onclick = function facebookopen() {
+			FB.ui({
+				method: "feed",
+				link: "http://www.discoveryvip.com",
+				picture: "http://www.discoveryvip.com/assets/img/siteimage.jpg",
+				name: "Play the quiz to win",
+				caption: "",
+				description: "I scored " + score + " correct out of " + totalquestions + " questions on the quiz"
+			});
+		};
 
-btnTw.onclick = function twitteropen() {
-	description = "I scored " + score + " correct out of " + totalquestions + " questions on the quiz";
-	shareurl = 'https://twitter.com/intent/tweet?text=' + encodeURI(description) + '&url=http://www.discoveryvip.com';
-	window.open(shareurl, 'Share window', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
-};
+		btnTw.onclick = function twitteropen() {
+			description = "I scored " + score + " correct out of " + totalquestions + " questions on the quiz";
+			shareurl = 'https://twitter.com/intent/tweet?text=' + encodeURI(description) + '&url=http://www.discoveryvip.com';
+			window.open(shareurl, 'Share window', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
+		};
 
-};
+	};
 })();
--- js/social.js
+++ js/social.js
@@ -1,77 +1,82 @@
+(function() {
 
-var metatags = document.getElementsByTagName("meta");
-var shareurl = '';
+	window.onload = function() {
+		var btnTw = document.getElementById("twitter-btn");
+                var btnFb = document.getElementById("facebook-btn");
+		//onclick="socialShare('twitter'); return false;"
+                btnTw.onclick = socialShare('twitter');
+                
+                
+		var metatags = document.getElementsByTagName("meta");
+		var shareurl = '';
 
-function getMetaTagInfo(tag) {
-	for (metatags, a = 0; a < metatags.length; a++) {
-		if (tag == metatags[a].name || tag == metatags[a].getAttribute("property")) {
-			return metatags[a].content;
+		function getMetaTagInfo(tag) {
+			for (metatags, a = 0; a < metatags.length; a++) {
+				if (tag == metatags[a].name || tag == metatags[a].getAttribute("property")) {
+					return metatags[a].content;
+				}
+			}
+			return false;
 		}
-	}
-	return false;
-}
 
 
-function facebookopen(){
-        FB.ui({
-          method: 'feed',
-          link: getMetaTagInfo("og:url"),
-          picture: getMetaTagInfo("og:image"),
-          name: getMetaTagInfo("og:title"),
-          caption: '',
-          description: getMetaTagInfo("og:description")
-      });
-}
+		btnFb.onclick =  function facebookopen() {
+			FB.ui({
+				method: 'feed',
+				link: getMetaTagInfo("og:url"),
+				picture: getMetaTagInfo("og:image"),
+				name: getMetaTagInfo("og:title"),
+				caption: '',
+				description: getMetaTagInfo("og:description")
+			});
+		};
 
 
-
-
-
-
-
-function socialShare(socialnetwork) {
-	console.log(socialnetwork);
-	//
-	var url = getMetaTagInfo("og:url"); //'http://www.discoveryvip.com/'; //window.location.href;
-	var title = getMetaTagInfo("og:title");
-	var description = getMetaTagInfo("og:description");
-	var image = getMetaTagInfo("og:image");
-	switch (socialnetwork) {
-		case 'twitter':
-			shareurl = 'https://twitter.com/intent/tweet?text=this%20is%20my%20text&url=' + url;
-			break;
-		case 'facebook':
-			shareurl = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURI(url);
-			break;
-		case 'google':
-			shareurl = 'https://plus.google.com/share?url=' + url;
-			break;
-		case 'linkedin':
-			shareurl = 'https://www.linkedin.com/shareArticle?mini=true&url=' + url + '&title=' + encodeURI(title) + '&summary=' + encodeURI(description) + '&source=LinkedIn';
-			break;
-		case 'pinterest':
-			shareurl = 'http://pinterest.com/pin/create/button/?url=' + url + '&media=' + image + '&description=' + encodeURI(description) + '&source=LinkedIn';
-			break;
-		case 'reddit':
-			shareurl = 'http://www.reddit.com/submit?url=' + url + '&title=' + encodeURI(title);
-			break;
-		case 'tumblr':
-			shareurl = 'http://www.tumblr.com/share?v=3&u=' + url + '&t=' + encodeURI(title);
-			break;
-		case 'stumbleupon':
-			shareurl = 'http://www.stumbleupon.com/submit?url=' + url + '&title=' + encodeURI(title);
-			break;
-		case 'slashdot':
-			shareurl = 'http://slashdot.org/bookmark.pl?url=' + url + '&title=' + encodeURI(title);
-			break;
-		case 'delicious':
-			shareurl = 'http://del.icio.us/post?url=' + url + '&title=' + title + '&notes=' + encodeURI(description);
-			break;
-		default:
-			shareurl = false;
-	}
-	if (shareurl) {
-		window.open(shareurl, 'Share window', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
-	}
-	console.log(shareurl);
-}
+		function socialShare(socialnetwork) {
+			console.log(socialnetwork);
+			//
+			var url = getMetaTagInfo("og:url"); //'http://www.discoveryvip.com/'; //window.location.href;
+			var title = getMetaTagInfo("og:title");
+			var description = getMetaTagInfo("og:description");
+			var image = getMetaTagInfo("og:image");
+			switch (socialnetwork) {
+				case 'twitter':
+					shareurl = 'https://twitter.com/intent/tweet?text=this%20is%20my%20text&url=' + url;
+					break;
+				case 'facebook':
+					shareurl = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURI(url);
+					break;
+				case 'google':
+					shareurl = 'https://plus.google.com/share?url=' + url;
+					break;
+				case 'linkedin':
+					shareurl = 'https://www.linkedin.com/shareArticle?mini=true&url=' + url + '&title=' + encodeURI(title) + '&summary=' + encodeURI(description) + '&source=LinkedIn';
+					break;
+				case 'pinterest':
+					shareurl = 'http://pinterest.com/pin/create/button/?url=' + url + '&media=' + image + '&description=' + encodeURI(description) + '&source=LinkedIn';
+					break;
+				case 'reddit':
+					shareurl = 'http://www.reddit.com/submit?url=' + url + '&title=' + encodeURI(title);
+					break;
+				case 'tumblr':
+					shareurl = 'http://www.tumblr.com/share?v=3&u=' + url + '&t=' + encodeURI(title);
+					break;
+				case 'stumbleupon':
+					shareurl = 'http://www.stumbleupon.com/submit?url=' + url + '&title=' + encodeURI(title);
+					break;
+				case 'slashdot':
+					shareurl = 'http://slashdot.org/bookmark.pl?url=' + url + '&title=' + encodeURI(title);
+					break;
+				case 'delicious':
+					shareurl = 'http://del.icio.us/post?url=' + url + '&title=' + title + '&notes=' + encodeURI(description);
+					break;
+				default:
+					shareurl = false;
+			}
+			if (shareurl) {
+				window.open(shareurl, 'Share window', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
+			}
+			console.log(shareurl);
+		}
+	};
+})();
